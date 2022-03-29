396c07f82bcf9eba123cb07639fdd16f4e1f250c
Shahrukh Mullani <mullanishah@gmail.com>
2022-03-29 23:53:16 +0530

    feature update as well as search student implemented

--- src/com/stud/dao/StudentDaoImpl.java
+++ src/com/stud/dao/StudentDaoImpl.java
@@ -1,5 +1,6 @@
 package com.stud.dao;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,17 +17,26 @@ import com.stud.utils.DBUtils;
 public class StudentDaoImpl implements StudentDao {
 	
 	private Connection conn;
-	private PreparedStatement pstGetStudDetails, pstAddStudDetails, pstRemoveStudDetails;
+	private PreparedStatement pstGetStudDetails, pstAddStudDetails, pstRemoveStudDetails, pstUpdateStudDetails;
+	private CallableStatement cstmtSearchStudent, cstmtVerifyStudent;
 	
 	public StudentDaoImpl() throws Exception {
 		this.conn = DBUtils.getConnection();
 		this.pstGetStudDetails = conn.prepareStatement(QueryHelper.queryGetStudDetails);
 		this.pstAddStudDetails = conn.prepareStatement(QueryHelper.queryAddStudDetails);
 		this.pstRemoveStudDetails = conn.prepareStatement(QueryHelper.queryRemoveStudDetails);
+		this.pstUpdateStudDetails = conn.prepareStatement(QueryHelper.queryUpdateStudDetails);
+		this.cstmtSearchStudent = conn.prepareCall(QueryHelper.callSearchStud);
+		this.cstmtVerifyStudent = conn.prepareCall(QueryHelper.callVerifyStud);
 	}
 
 	public void cleanUp() throws Exception {
-		
+		if(null != cstmtVerifyStudent)
+			cstmtVerifyStudent.close();
+		if(null != cstmtSearchStudent)
+			cstmtSearchStudent.close();
+		if(null != pstUpdateStudDetails)
+			pstUpdateStudDetails.close();
 		if(null != pstRemoveStudDetails)
 			pstRemoveStudDetails.close();
 		if(null != pstAddStudDetails)
@@ -81,10 +91,58 @@ public class StudentDaoImpl implements StudentDao {
 		pstRemoveStudDetails.setString(2, studName);
 		
 		int deleteCount = pstRemoveStudDetails.executeUpdate();
-		if(deleteCount > 0) {
+		if(deleteCount > 0)
 			return QueryHelper.deletionSucceed;
-		}
 		return QueryHelper.deletionFailed;
 	}
 
+	@Override
+	public String updateStudentDetails(long id, Student student) throws Exception {
+
+		pstUpdateStudDetails.setString(1, student.getFullName());
+		pstUpdateStudDetails.setString(2, student.getCourseTitle());
+		pstUpdateStudDetails.setString(3, student.getCurrentSem());
+		pstUpdateStudDetails.setString(4, student.getAddress());
+		pstUpdateStudDetails.setDate(5, new java.sql.Date(student.getBirthDate().getTime()));
+		pstUpdateStudDetails.setLong(6, student.getContactNum());
+		pstUpdateStudDetails.setLong(7, student.getEmergencyContact());
+		pstUpdateStudDetails.setLong(8, id);
+		
+		int updateCount = pstUpdateStudDetails.executeUpdate();
+		if(updateCount > 0)
+			return QueryHelper.updationSucceed;
+		return QueryHelper.updationFailed;
+	}
+
+	@Override
+	public Student searchStudent(long studId) throws Exception {
+		
+		Student student = null;
+		cstmtSearchStudent.setLong(1, studId);
+		ResultSet rst = cstmtSearchStudent.executeQuery();
+		if(rst.next()) {
+			student = new Student();
+			student.setStudId(rst.getLong(1));
+			student.setFullName(rst.getObject(2).toString());
+			student.setCourseTitle(rst.getObject(3).toString());
+			student.setCurrentSem(rst.getString(4));
+			student.setAddress(rst.getObject(5).toString());
+			student.setBirthDate(rst.getDate(6));
+			student.setContactNum(rst.getLong(7));
+			student.setEmergencyContact(Long.valueOf(rst.getObject(8).toString()));
+			return student;
+		}
+		return null;
+	}
+
+	@Override
+	public Boolean verifyStudent(long studId) throws Exception {
+		
+		cstmtVerifyStudent.setLong(1, studId);
+		cstmtVerifyStudent.registerOutParameter(2, java.sql.Types.BOOLEAN);
+		cstmtVerifyStudent.execute();
+		
+		boolean isIdPresent = cstmtVerifyStudent.getBoolean(2);
+		return isIdPresent;
+	}	
 }
